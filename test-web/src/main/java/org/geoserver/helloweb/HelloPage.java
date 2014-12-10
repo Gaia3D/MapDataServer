8ea05239e072a87053b5423b4eb5744fe0eb4c75
BJ Jang <qgis.tr.kr@gmail.com>
2014-12-10 20:01:55 +0900

    init test prj

---
+++ test-web/src/main/java/org/geoserver/helloweb/HelloPage.java
@@ -0,0 +1,10 @@
+package org.geoserver.helloweb;
+
+import org.apache.wicket.markup.html.basic.Label;
+import org.geoserver.web.GeoServerBasePage;
+
+public class HelloPage extends GeoServerBasePage {
+	public HelloPage() {
+		add(new Label("hellolabel", "Hello World!"));
+	}
+}
